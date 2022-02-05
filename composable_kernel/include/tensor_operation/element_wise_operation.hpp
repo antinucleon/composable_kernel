3acf8eb6ac6945c999a3a83efef39ea7fa0bb70c
Bing Xu <bingxu@fb.com>
2022-02-05 12:43:52 -0800

    asdf

--- composable_kernel/include/tensor_operation/element_wise_operation.hpp
+++ composable_kernel/include/tensor_operation/element_wise_operation.hpp
@@ -14,21 +14,42 @@ struct PassThrough
     }
 };
 
+struct AddHardSwish {
+    template <typename T>
+    __host__ __device__ constexpr void operator()(T& y, const T& x0, const T& x1) const
+    {
+
+        // hack for Hardswich
+        float a = x0 + x1;
+        float b = a + 3.f;
+        float c = (b > 0.f) * (b > 6.0f ? 6.0f : b) * a * 0.166667f;
+        y       = c;
+    }
+
+};
+
+struct AddHardSwishAdd {
+    template <typename T>
+    __host__ __device__ constexpr void operator()(T& y, const T& x0, const T& x1, const T& x2) const
+    {
+
+
+        float a = x0 + x1;
+        float b = a + 3.f;
+        float c = (b > 0.0f) * (b > 6.0f ? 6.0f : b) * a * 0.166667f;
+        float d = c + x2;
+        y       = d;
+    }
+};
 struct AddRelu
 {
     template <typename T>
     __host__ __device__ constexpr void operator()(T& y, const T& x0, const T& x1) const
     {
-#if 0
+
         T a = x0 + x1;
         y   = a > 0 ? a : 0;
-#else
-        // hack for Hardswich
-        float a = x0 + x1;
-        float b = a + float{3};
-        float c = (b > 0) * (b > float{6} ? float{6} : b) * a * float{0.166667};
-        y       = c;
-#endif
+
     }
 };
 
@@ -37,18 +58,11 @@ struct AddReluAdd
     template <typename T>
     __host__ __device__ constexpr void operator()(T& y, const T& x0, const T& x1, const T& x2) const
     {
-#if 0
+
         T a = x0 + x1;
         T b = a > 0 ? a : 0;
         y   = b + x2;
-#else
-        // hack for Hardswich
-        float a = x0 + x1;
-        float b = a + float{3};
-        float c = (b > 0) * (b > float{6} ? float{6} : b) * a * float{0.166667};
-        float d = c + x2;
-        y       = d;
-#endif
+
     }
 };
 
